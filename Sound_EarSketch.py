9d00d96865d5432d30a0437f61561eb98a935acb
João Guilherme <joaoguilherme9254@gmail.com>
2021-07-14 15:10:04 -0300

    Create Sound_EarSketch.py

---
+++ Sound_EarSketch.py
@@ -0,0 +1,14 @@
+from earsketch import *
+
+init()
+setTempo(105)
+sounds = [Y43_PERCUSSION_1,MILKNSIZZ_ADIOS_BOTTOM_KICK,HOUSE_DEEP_PIANO_001,RD_CINEMATIC_SCORE_STRINGS_16,RD_CINEMATIC_SCORE_DRUMPART_23,RD_CINEMATIC_SCORE_STRINGS_1]
+
+fitMedia(sounds[0],1,1,19)
+fitMedia(sounds[1],2,11,21)
+fitMedia(sounds[2],3,19,33)
+fitMedia(sounds[5],4,19,42)
+fitMedia(sounds[4],2,45,73)
+fitMedia(sounds[2],3,40,66)
+fitMedia(sounds[3],4,47,63)
+finish()
